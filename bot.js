cff70de4e7d8090b0e0b4fbc92ae6587c376c67b
UnDied <49878990+UnDied@users.noreply.github.com>
2019-04-23 20:47:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,6 @@ const rcon = new Rcon("127.0.0.1" /* ← Ркон айпи*/, 25569 /* ← Рк�
 let token = "NTY4NDk5OTA3MzY4MDU4ODgx.XL9Now.MF_tQC4gYYG6VvLfCEKyu7mzMtQ";
 client.login(token);
 
-client.user.setActivity("Type /cmd!", { type: "WATCHING"});
 client.on('message', msg => {
 	if (msg.author.bot) return;
 	if (msg.channel.type.toLowerCase() == 'dm') {
